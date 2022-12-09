962bdf99ede45a6208adcb7d14d010ddec21a7a4
Your Name <you@example.com>
2022-12-09 10:24:36 -0500

    Got js for product seach working.

---
+++ scripts/products.js
@@ -0,0 +1,81 @@
+"use strict";
+
+let filterSelect = document.getElementById("filterSelect");
+let bevTypeDropdown = document.getElementById("bevTypeDropdown");
+let productTable = document.getElementById("productTable");
+let productTableBody = document.getElementById("productTableBody");
+
+window.onload = function () {
+  filterSelect.onchange = FilterSelectOnChange;
+  bevTypeDropdown.onchange = BevTypeDropdownOnChange;
+};
+
+function FilterSelectOnChange() {
+  if (filterSelect.value == "searchByCategory") {
+    initCategorySelect(bevTypeDropdown);
+    bevTypeDropdown.style.display = "block";
+  } else if (filterSelect.value == "viewAll") {
+    clearTable(productTableBody);
+    bevTypeDropdown.style.display = "none";
+    productTable.style.display = "block";
+    fetch(`http://localhost:8081/api/products`)
+      .then((response) => response.json())
+      .then((data) => {
+        for (let x of data) {
+          let row = productTableBody.insertRow(-1);
+          let cell1 = row.insertCell(0);
+          let cell2 = row.insertCell(1);
+          let cell3 = row.insertCell(2);
+          cell1.innerHTML = `<a href="details.html?productId=${x.productId}">${x.productName}</a>`;
+          cell2.innerHTML = `$${parseFloat(x.unitPrice).toFixed(2)}`;
+          cell3.innerHTML = x.productId;
+        }
+      });
+  } else if (filterSelect.value == "selectOne") {
+    bevTypeDropdown.style.display = "none";
+    productTable.style.display = "none";
+  }
+}
+
+function initCategorySelect(select) {
+  select.length = 0;
+  let option = new Option("Select a category", "select");
+  select.appendChild(option);
+  fetch(`http://localhost:8081/api/categories`)
+    .then((response) => response.json())
+    .then((data) => {
+      for (let x of data) {
+        let option = new Option(x.name, x.categoryId);
+        select.appendChild(option);
+      }
+    });
+}
+
+function BevTypeDropdownOnChange() {
+  clearTable(productTableBody);
+
+  if (bevTypeDropdown.value == "select") {
+    productTable.style.display = "none";
+  } else {
+    productTable.style.display = "block";
+    fetch(`http://localhost:8081/api/products`)
+      .then((response) => response.json())
+      .then((data) => {
+        for (let x of data) {
+          if (bevTypeDropdown.value == x.categoryId) {
+            let row = productTableBody.insertRow(-1);
+            let cell1 = row.insertCell(0);
+            let cell2 = row.insertCell(1);
+            let cell3 = row.insertCell(2);
+            cell1.innerHTML = `<a href="productdetails.html?productId=${x.productId}">${x.productName}</a>`;
+            cell2.innerHTML = `$${parseFloat(x.unitPrice).toFixed(2)}`;
+            cell3.innerHTML = x.productId;
+          }
+        }
+      });
+  }
+}
+
+function clearTable(table) {
+  table.replaceChildren();
+}
